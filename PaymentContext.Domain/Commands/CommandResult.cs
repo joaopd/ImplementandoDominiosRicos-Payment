d7ca27e49d28b137205dcd03118acf8fbcd5b6bd
joao pedro correia <jpkabral@live.com>
2021-08-23 23:30:46 -0300

    Handle

---
+++ PaymentContext.Domain/Commands/CommandResult.cs
@@ -0,0 +1,26 @@
+﻿using PaymentContext.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CommandResult : ICommandReult
+    {
+        public CommandResult()
+        {
+
+        }
+        public CommandResult(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+    }
+}
---
+++ PaymentContext.Shared/Handler/IHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Shared.Handler
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandReult Handle(T comand);
+    } 
+}
