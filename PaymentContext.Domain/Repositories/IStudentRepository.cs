da042619f06d9dc87d46c825bb358a462244934c
joao pedro correia <jpkabral@live.com>
2021-08-23 23:19:03 -0300

    repository parttnes

---
+++ PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using PaymentContext.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain.Repositories
+{
+    public interface IStudentRepository
+    {
+        bool DocumentExist(string document);
+        bool EmailExist(string email);
+        bool CreateSubscription(Student student);
+    }
+}
